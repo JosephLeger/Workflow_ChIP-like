bf86c78f5121b8bf54909977a3dccf186648ea60
JosephLeger <95336622+JosephLeger@users.noreply.github.com>
2024-02-19 16:30:39 +0100

    Update mergeBAM.sh
    - write report properly (not tested yet)

--- script/mergeBAM.sh
+++ script/mergeBAM.sh
@@ -118,6 +118,15 @@ module load samtools/1.15.1
 echo '#' >> ./0K_REPORT.txt
 date >> ./0K_REPORT.txt
 
+Launch()
+{
+# Launch COMMAND and save report
+echo -e "#$ -V \n#$ -cwd \n#$ -S /bin/bash \n"${COMMAND} | qsub -N ${JOBNAME} ${WAIT}
+echo -e ${JOBNAME} >> ./0K_REPORT.txt
+echo -e ${COMMAND} | sed 's@^@   \| @' >> ./0K_REPORT.txt
+}
+WAIT=''
+
 # Establish conditions_list which contains already visited condition2 (celltype)
 conditions_list=""
 while IFS=',' read -r sra filename cond1 cond2; do
@@ -143,14 +152,11 @@ while IFS=',' read -r sra filename cond1 cond2; do
                 sra_files="${sra_files} ${newfile}"
             fi
         done < ${2}
-        # Launch qsub    
-        echo -e "#$ -V \n#$ -cwd \n#$ -S /bin/bash \n\
-        samtools merge -o ${1}/${M_arg}_${cond2}${newsuffix} ${sra_files} \n\
-        samtools index ${1}/${M_arg}_${cond2}${newsuffix}" | qsub -N mergeBAM_${M_arg}_${cond2}
-        # Update REPORT
-        echo -e "mergeBAM_${M_arg}_${cond2} | fastqc -o QC/${input} --noextract -f fastq $i" >> ./0K_REPORT.txt
-        echo -e "        | samtools merge -o ${1}/${M_arg}_${cond2}${newsuffix} ${sra_files}" >> ./0K_REPORT.txt
-        echo -e "        | samtools index ${1}/${M_arg}_${cond2}${newsuffix}" >> ./0K_REPORT.txt
-        
+
+        ## Define JOB and COMMAND and launch job
+        JOBNAME="mergeBAM_${M_arg}_${cond2}"
+        COMMAND="samtools merge -o ${1}/${M_arg}_${cond2}${newsuffix} ${sra_files} \n\
+        samtools index ${1}/${M_arg}_${cond2}${newsuffix}"
+        Launch        
     fi
 done < ${2}
