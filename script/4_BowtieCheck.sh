8c2dd5a106c8e8031e9117ed3310924e4f085e44
JosephLeger <95336622+JosephLeger@users.noreply.github.com>
2024-05-16 09:46:51 +0200

    Update 4_BowtieCheck.sh

--- script/4_BowtieCheck.sh
+++ script/4_BowtieCheck.sh
@@ -18,10 +18,10 @@ Help()
 {
 echo -e "${BOLD}####### FILTER MANUAL #######${END}\n\n\
 ${BOLD}SYNTHAX${END}\n\
-    sh ${script_name} [options] <input_dir1> <...>\n\n\
+    sh ${script_name} [options] <input_dir>\n\n\
 
 ${BOLD}DESCRIPTION${END}\n\
-    Perform identification of duplicated and low quality reads in sorted BAM files and remove them.\n\
+    Perform identification of duplicated and low quality reads in sorted BAM files and remove them using Picard.\n\
     It generates '_Duplist_<filename>.txt' which contains duplicates list and '<filename>_unique_filtered.bam' files.\n\n\
 
 ${BOLD}OPTIONS${END}\n\
@@ -32,16 +32,13 @@ ${BOLD}OPTIONS${END}\n\
         Define quality threshold for read filtering.\n\
         Default = 10\n\n\
     ${BOLD}-R${END} ${UDL}boolean${END}, ${BOLD}R${END}emoveDuplicates\n\
-        Whether remove duplicated or not.\n\
+        Whether remove duplicated reads or not.\n\
         Default = False\n\n\
 
 ${BOLD}ARGUMENTS${END}\n\
     ${BOLD}<input_dir>${END}\n\
         Directory containing .bam files to process.\n\
         It usually corresponds to 'Mapped/<model>/BAM'.\n\n\
-
-    ${BOLD}<...>${END}\n\
-        Several directories can be specified as argument in the same command line, allowing processing of multiple models simultaneously.\n\n\  
 
 ${BOLD}EXAMPLE USAGE${END}\n\
     sh ${script_name} ${BOLD}-N${END} _sorted ${BOLD}-T${END} 10 ${BOLD}-R${END} False ${BOLD}Mapped/mm39/BAM${END}\n"
@@ -57,12 +54,12 @@ T_arg=10
 R_arg='False'
 
 # Change default values if another one is precised
-while getopts ":T:N:R:" option; do
+while getopts ":N:T:R:" option; do
     case $option in
+	 N) # NAME OF FILES (SUFFIX)
+            N_arg=${OPTARG};;
         T) # THRESHOLD FOR FILTERING
             T_arg=${OPTARG};;
-        N) # NAME OF FILES (SUFFIX)
-            N_arg=${OPTARG};;
         R) # REMOVE DUPLICATES
             R_arg=${OPTARG};;
         \?) # Error
@@ -90,32 +87,25 @@ case $R_arg in
         exit;;
 esac
 
-# Deal with options [-N|-T|-R] and arguments [$1|$2|...]
+# Deal with options [-N|-T|-R] and argument [$1]
 shift $((OPTIND-1))
 
 ################################################################################################################
-### ERRORS ----------------------------------------------------------------------------------------------------''
+### ERRORS -----------------------------------------------------------------------------------------------------
 ################################################################################################################
 
 if [ $# -eq 1 ] && [ $1 == "help" ]; then
         Help
         exit
-elif [ $# == 0 ]; then
+elif [ $# -lt 1 ]; then
         # Error if inoccrect number of agruments is provided
         echo "Error synthax : please use following synthax"
-        echo "      sh ${script_name} [options] <input_dir1> <...>"
+        echo "      sh ${script_name} [options] <input_dir> <...>"
         exit
-else
-    # For each input file given as argument
-    for input in "$@"; do
-        # Count .bam files matching -N pattern in provided directory
-        files=$(shopt -s nullglob dotglob; echo ${input}/*${N_arg}*.bam)
-        if (( !${#files} )); then
-            # Error if current provided directory is empty or does not exists
-            echo -e "Error : can not find files to filter in ${input} directory. Please make sure the provided input directory exists, and contains sorted .bam files."
-            exit
-        fi
-    done
+elif [ $(ls $1/*${N_arg}*.bam 2>/dev/null | wc -l) -lt 1 ]; then
+	# Error if provided directory is empty or does not exists
+	echo -e "Error : can not find files to filter in ${input} directory. Please make sure the provided input directory exists, and contains sorted .bam files."
+	exit
 fi
 
 ################################################################################################################
@@ -140,32 +130,31 @@ echo -e ${COMMAND} | sed -r 's@\|@\n@g' | sed 's@^@   \| @' >> ./0K_REPORT.txt
 WAIT=''
 
 ## BOWTIE CHECK - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-# For each input file given as argument
-for input in "$@"; do   
-    # Precise to eliminate empty lists for the loop
-    shopt -s nullglob
-    for i in ${input}/*${N_arg}*.bam; do
-        # Set variables for the run :
-        model=`echo ${input} | sed -e 's@.*Mapped\/@@g' | sed -e 's@\/.*@@g'`
-        current_file=`echo $i | sed -e "s@${input}\/@@" | sed -e 's@\.bam@@g'`
-        # Define JOBNAME and COMMAND and launch job
-        if [ ${R_arg} == 'true' ]; then
-            JOBNAME="BowtieCheck_${model}_${current_file}"
-            COMMAND="picard MarkDuplicates INPUT=${i} \
-            OUTPUT=${input}/${current_file}_unique.bam \
-            VALIDATION_STRINGENCY=LENIENT \
-            TMP_DIR=/tmp \
-            METRICS_FILE=${input}/_DupList_${current_file}.txt \
-            REMOVE_DUPLICATES=true \n\
-            samtools view -h ${input}/${current_file}_unique.bam | samtools view -b -Sq ${T_arg} > ${input}/${current_file}_unique_filtered.bam \n\
-            samtools index ${input}/${current_file}_unique_filtered.bam ${input}/${current_file}_unique_filtered.bai"
-            Launch 
+
+# Precise to eliminate empty lists for the loop
+shopt -s nullglob
+# For each BAM file in provided directory
+for file in ${1}/*${N_arg}*.bam; do
+	# Set variables for the run :
+	model=`echo ${1} | sed -e 's@.*Mapped\/@@g' | sed -e 's@\/.*@@g'`
+	current_file=`echo ${file} | sed -e "s@${1}\/@@" | sed -e 's@\.bam@@g'`
+	# Define JOBNAME and COMMAND and launch job
+	if [ ${R_arg} == 'true' ]; then
+		JOBNAME="BowtieCheck_${model}_${current_file}"
+		COMMAND="picard MarkDuplicates INPUT=${i} \
+		OUTPUT=${1}/${current_file}_unique.bam \
+		VALIDATION_STRINGENCY=LENIENT \
+		TMP_DIR=/tmp \
+		METRICS_FILE=${1}/_DupList_${current_file}.txt \
+ 		REMOVE_DUPLICATES=true \n\
+		samtools view -h ${1}/${current_file}_unique.bam | samtools view -b -Sq ${T_arg} > ${1}/${current_file}_unique_filtered.bam \n\
+		samtools index ${1}/${current_file}_unique_filtered.bam ${1}/${current_file}_unique_filtered.bai"
+		Launch 
         else 
-            JOBNAME="BowtieCheck_${model}_${current_file}"
-            COMMAND="samtools view -h ${i} | samtools view -b -Sq ${T_arg} > ${input}/${current_file}_filtered.bam \n\
-            samtools index ${input}/${current_file}_filtered.bam ${input}/${current_file}_filtered.bai"
-            Launch
+		JOBNAME="BowtieCheck_${model}_${current_file}"
+		COMMAND="samtools view -h ${file} | samtools view -b -Sq ${T_arg} > ${1}/${current_file}_filtered.bam \n\
+		samtools index ${1}/${current_file}_filtered.bam ${1}/${current_file}_filtered.bai"
+		Launch
         fi
-    done
 done
           
