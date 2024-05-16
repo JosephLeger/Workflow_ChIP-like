5f6d92baeef3b139ffc60c05887e36b1374cf826
JosephLeger <95336622+JosephLeger@users.noreply.github.com>
2024-05-16 10:06:39 +0200

    Update 5_PeakyFinders.sh

--- script/5_PeakyFinders.sh
+++ script/5_PeakyFinders.sh
@@ -18,84 +18,84 @@ Help()
 {
 echo -e "${BOLD}####### PEAKYFINDER MANUAL #######${END}\n\n\
 ${BOLD}SYNTHAX${END}\n\
-    sh ${script_name} [options] <input_dir> <chrom_size>\n\n\
+	sh ${script_name} [options] <input_dir> <chrom_size>\n\n\
 
 ${BOLD}DESCRIPTION${END}\n\
-    Perform peak calling from BAM files using HOMER or MACS2.\n\
-    Peaks thus called are saved in a .txt or .narrowPeak file, sorted and then saved in .bed .bedgraph and .bw files. \n\
-    It creates new folders './HOMER/Tags/<tag_name>' and './HOMER/Peaks/<tag_name>' or './MACS2/Peaks/<tag_name>' in which output files are stored.\n\n\
+	Perform peak calling from BAM files using HOMER or MACS2.\n\
+	Peaks thus called are saved in a .txt or .narrowPeak file, sorted and then saved in .bed .bedgraph and .bw files. \n\
+	It creates new folders './HOMER/Tags/<tag_name>' and './HOMER/Peaks/<tag_name>' or './MACS2/Peaks/<tag_name>' in which output files are stored.\n\n\
 
 ${BOLD}OPTIONS${END}\n\n\
 
 ${BOLD}Common Options${END}\n\
-    ${BOLD}-N${END} ${UDL}suffix${END}, ${BOLD}N${END}amePattern\n\
-        Define a suffix that input files must share to be considered. Allows to exclude BAM files that are unfiltered or unwanted.\n\
-        Default = '_filtered'\n\n\
-    ${BOLD}-U${END} ${UDL}toolName${END}, ${BOLD}U${END}sedTool\n\
-        Define tool used for peak calling. Must be in 'HOMER' or 'MACS2'.\n\
-        Default = 'HOMER'\n\n\
+	${BOLD}-N${END} ${UDL}suffix${END}, ${BOLD}N${END}amePattern\n\
+		Define a suffix that input files must share to be considered. Allows to exclude BAM files that are unfiltered or unwanted.\n\
+		Default = '_filtered'\n\n\
+	${BOLD}-U${END} ${UDL}toolName${END}, ${BOLD}U${END}sedTool\n\
+		Define tool used for peak calling. Must be in 'HOMER' or 'MACS2'.\n\
+		Default = 'HOMER'\n\n\
 
 ${BOLD}HOMER Options${END}\n\
-    ${BOLD}-S${END} ${UDL}size${END}, fragment${BOLD}S${END}ize\n\
-        Define fragment size sequenced.\n\
-        Default = auto\n\n\
-    ${BOLD}-M${END} ${UDL}style${END}, ${BOLD}M${END}ode\n\
-        Precise mode (HOMER -style) to use when running script.\n\
-        Must be in 'factor', 'histone', 'super', 'groseq', 'tss', 'dnase' or 'mC'.\n\
-        Default = factor\n\n\
-    ${BOLD}-I${END} ${UDL}file${END}, ${BOLD}I${END}nputControl\n\
-        NOT FUNCTIONNAL YET\n\
-        THE IDEA IS TO PRECISE ORIGINATE BAM IGG FOLDER TO ASSUME FURTHER TAG FOLDER REQUIRED FOR THE COMMAND\n\
-        Specify a folder containing input control BAM files to use as reference background noize for peak calling.\n\
-        If setted, peaks are filtered based on threshold defined with -F option.\n\
-        It usually correspond to IgG experiment. \n\
-        Default = None\n\n\
-    ${BOLD}-F${END} ${UDL}threshold${END}, ${BOLD}F${END}oldEnrichmentVersusInput\n\
-        NOT FUNCTIONNAL YET\n\
-        Define required fold change of experimental data compared to input control to consider peaks.\n\
-        Default = 4\n\n\
-    ${BOLD}-L${END} ${UDL}threshold${END}, ${BOLD}L${END}ocalFoldEnrichment\n\
-        Define required fold change of a region compared to local signal (surrounding 10kb region) to consider peaks.\n\
-        Default = 4\n\n\
-    ${BOLD}-C${END} ${UDL}threshold${END}, ${BOLD}C${END}lonalFoldEnrichment\n\
-        Define maximal ratio for the number of unique positions containing tags in a peak relative to the expected number of unique positions given the total number of tags in the peak.\n\
-        For MNase or other restriction enzyme digestion, should be setted to 0.\n\
-        Default = 2\n\n\
-    ${BOLD}-T${END} ${UDL}threshold${END}, ${BOLD}T${END}agThreshold\n\
-        Define required minimal tag value to consider peaks.\n\
-        Default=2\n\n\
+	${BOLD}-S${END} ${UDL}size${END}, fragment${BOLD}S${END}ize\n\
+		Define fragment size sequenced.\n\
+		Default = auto\n\n\
+	${BOLD}-M${END} ${UDL}style${END}, ${BOLD}M${END}ode\n\
+		Precise mode (HOMER -style) to use when running script.\n\
+		Must be in 'factor', 'histone', 'super', 'groseq', 'tss', 'dnase' or 'mC'.\n\
+		Default = factor\n\n\
+	${BOLD}-I${END} ${UDL}file${END}, ${BOLD}I${END}nputControl\n\
+		NOT FUNCTIONNAL YET\n\
+		THE IDEA IS TO PRECISE ORIGINATE BAM IGG FOLDER TO ASSUME FURTHER TAG FOLDER REQUIRED FOR THE COMMAND\n\
+		Specify a folder containing input control BAM files to use as reference background noize for peak calling.\n\
+		If setted, peaks are filtered based on threshold defined with -F option.\n\
+		It usually correspond to IgG experiment. \n\
+		Default = None\n\n\
+	${BOLD}-F${END} ${UDL}threshold${END}, ${BOLD}F${END}oldEnrichmentVersusInput\n\
+		NOT FUNCTIONNAL YET\n\
+		Define required fold change of experimental data compared to input control to consider peaks.\n\
+		Default = 4\n\n\
+	${BOLD}-L${END} ${UDL}threshold${END}, ${BOLD}L${END}ocalFoldEnrichment\n\
+		Define required fold change of a region compared to local signal (surrounding 10kb region) to consider peaks.\n\
+		Default = 4\n\n\
+	${BOLD}-C${END} ${UDL}threshold${END}, ${BOLD}C${END}lonalFoldEnrichment\n\
+		Define maximal ratio for the number of unique positions containing tags in a peak relative to the expected number of unique positions given the total number of tags in the peak.\n\
+		For MNase or other restriction enzyme digestion, should be setted to 0.\n\
+		Default = 2\n\n\
+	${BOLD}-T${END} ${UDL}threshold${END}, ${BOLD}T${END}agThreshold\n\
+		Define required minimal tag value to consider peaks.\n\
+		Default=2\n\n\
 
-    For more details, please see HOMER manual \n\ 
-    (http://homer.ucsd.edu/homer/ngs/peaks.html)\n\n\
+	For more details, please see HOMER manual \n\ 
+	(http://homer.ucsd.edu/homer/ngs/peaks.html)\n\n\
     
 ${BOLD}MACS2 Options${END}\n\n\
-    ${BOLD}-G${END} ${UDL}size${END}, ${BOLD}G${END}enomSize\n\
-        Effective genome size. Default human = 2.7e9 ; Default mouse : 1.87e9.\n\
-        Default = 1.87e9\n\n\
-    ${BOLD}-H${END} ${UDL}integer${END}, S${BOLD}h${END}ift\n\
-        Value used to move cutting ends before applying ExtendSize parameter.\n\
-        Default = 50\n\n\
-    ${BOLD}-E${END} ${UDL}integer${END}, ${BOLD}E${END}xtendSize\n\
-        Extend reads by fixing fragment size to provided length.\n\
-        Default = 100\n\n\
+	${BOLD}-G${END} ${UDL}size${END}, ${BOLD}G${END}enomSize\n\
+		Effective genome size. Default human = 2.7e9 ; Default mouse : 1.87e9.\n\
+		Default = 1.87e9\n\n\
+	${BOLD}-H${END} ${UDL}integer${END}, S${BOLD}h${END}ift\n\
+		Value used to move cutting ends before applying ExtendSize parameter.\n\
+		Default = 50\n\n\
+	${BOLD}-E${END} ${UDL}integer${END}, ${BOLD}E${END}xtendSize\n\
+		Extend reads by fixing fragment size to provided length.\n\
+		Default = 100\n\n\
 
-    For more details, please see MACS2 manual \n\n\
+	For more details, please see MACS2 manual \n\n\
 
 ${BOLD}ARGUMENTS${END}\n\
-    ${BOLD}<input_dir>${END}\n\
-        Directory containing BAM files to process.\n\
-        It usually corresponds to 'Mapped/<model>/BAM'.\n\n\
-    ${BOLD}<chrom_size>${END}\n\
-        Pathway to chromosome size file.\n\
-        Could be downloaded on UCSC website (e.g. https://hgdownload.soe.ucsc.edu/goldenPath/mm39/bigZips/, 'mm39.chrom.sizes').\n\
-        For HOMER usage, downloaded file have to be modified as following :\n\
-            1) Remove 'chr' in front of chromosome names.\n\
-            2) Remove everything before and after '_' for non-usual chromosomes names.\n\
-            3) Replace 'v1' by '.1' in non-usual chromosme names.\n\
-            4) Make sure entries are Tab separated.\n\n\
+	${BOLD}<input_dir>${END}\n\
+		Directory containing BAM files to process.\n\
+		It usually corresponds to 'Mapped/<model>/BAM'.\n\n\
+	${BOLD}<chrom_size>${END}\n\
+		Pathway to chromosome size file.\n\
+		Could be downloaded on UCSC website (e.g. https://hgdownload.soe.ucsc.edu/goldenPath/mm39/bigZips/, 'mm39.chrom.sizes').\n\
+		For HOMER usage, downloaded file have to be modified as following :\n\
+			1) Remove 'chr' in front of chromosome names.\n\
+			2) Remove everything before and after '_' for non-usual chromosomes names.\n\
+			3) Replace 'v1' by '.1' in non-usual chromosme names.\n\
+			4) Make sure entries are Tab separated.\n\n\
     
 ${BOLD}EXAMPLE USAGE${END}\n\
-    sh ${script_name} -U 'HOMER' ${BOLD}-N${END} _unique_filtered ${BOLD}-S${END} 50 ${BOLD}-M${END} dnase ${BOLD}-I${END} none ${BOLD}-F${END} none ${BOLD}-L${END} 4 ${BOLD}-C${END} 2 ${BOLD}$Mapped/mm39/BAM ${usr}/Ref/Genome/mm39.chrom.sizes${END}\n"
+	sh ${script_name} -U 'HOMER' ${BOLD}-N${END} _unique_filtered ${BOLD}-S${END} 50 ${BOLD}-M${END} dnase ${BOLD}-I${END} none ${BOLD}-F${END} none ${BOLD}-L${END} 4 ${BOLD}-C${END} 2 ${BOLD}$Mapped/mm39/BAM ${usr}/Ref/Genome/mm39.chrom.sizes${END}\n"
 }
 
 ################################################################################################################
@@ -121,56 +121,56 @@ E_arg=100
 
 # Change default values if another one is precised
 while getopts ":N:U:S:M:I:F:L:C:T:G:H:E:" option; do
-    case $option in
-        N) # NAME OF FILE (SUFFIX)
-            N_arg=${OPTARG};;
-        U) # USED TOOL FOR PEAK CALLING
-            U_arg=${OPTARG};;
-        S) # SIZE OF FRAGMENTS
-            S_arg=${OPTARG};;
-        M) # MODE TO RUN FIND PEAKS (STYLE)
-            M_arg=${OPTARG};;
-        I) # INPUT CONTROL (IgG)
-            I_arg=${OPTARG};;
-        F) # FOLD CHANGE THAN CONTROL THRESHOLD (IgG)
-            F_arg=${OPTARG};;
-        L) # LOCAL TAG FOLD CHANGE THRESHOLD
-            L_arg=${OPTARG};;
-        C) # CLONAL SIGNAL THRESHOLD
-            C_arg=${OPTARG};;
-        T) # TAG THRESHOLD
-            T_arg=${OPTARG};;
-        G) # MACS2 GENOME SIZE
-            G_arg=${OPTARG};;
-        H) # MACS2 SHIFT
-            H_arg=${OPTARG};;
-        E) # MAC2 EXTEND
-            E_arg=${OPTARG};;
-        \?) # Error
-            echo "Error : invalid option"
-            echo "      Allowed options are [-N|-U|-S|-M|-I|-F|-L|-C|-T|-G|-H|-E]"
-            echo "      Enter 'sh ${script_name} help' for more details"
-            exit;;
-        esac
+	case $option in
+		N) # NAME OF FILE (SUFFIX)
+			N_arg=${OPTARG};;
+		U) # USED TOOL FOR PEAK CALLING
+			U_arg=${OPTARG};;
+		S) # SIZE OF FRAGMENTS
+			S_arg=${OPTARG};;
+		M) # MODE TO RUN FIND PEAKS (STYLE)
+			M_arg=${OPTARG};;
+		I) # INPUT CONTROL (IgG)
+			I_arg=${OPTARG};;
+		F) # FOLD CHANGE THAN CONTROL THRESHOLD (IgG)
+			F_arg=${OPTARG};;
+		L) # LOCAL TAG FOLD CHANGE THRESHOLD
+			L_arg=${OPTARG};;
+		C) # CLONAL SIGNAL THRESHOLD
+			C_arg=${OPTARG};;
+		T) # TAG THRESHOLD
+			T_arg=${OPTARG};;
+		G) # MACS2 GENOME SIZE
+			G_arg=${OPTARG};;
+		H) # MACS2 SHIFT
+			H_arg=${OPTARG};;
+		E) # MAC2 EXTEND
+			E_arg=${OPTARG};;
+		\?) # Error
+			echo "Error : invalid option"
+			echo "      Allowed options are [-N|-U|-S|-M|-I|-F|-L|-C|-T|-G|-H|-E]"
+			echo "      Enter 'sh ${script_name} help' for more details"
+			exit;;
+	esac
 done
 
 # Checking if provided option values are correct
 case $I_arg in
-    None|none|FALSE|False|false|F) 
-        I_arg=''
-        F_arg='';;
-    *) 
-        I_arg='-i '$I_arg
-        F_arg='-F '$F_arg' ';;
+	None|none|FALSE|False|false|F) 
+		I_arg=''
+		F_arg='';;
+	*) 
+		I_arg='-i '$I_arg
+		F_arg='-F '$F_arg' ';;
 esac
 case $U_arg in
-    HOMER|homer|Homer) 
-        U_arg='HOMER';;
-    MACS2|mac2|Macs2)
-        U_arg='MACS2';;
-    *) 
-        echo "Error value : -U argument must be 'HOMER' or 'MACS2'"
-        exit;;
+	HOMER|homer|Homer) 
+		U_arg='HOMER';;
+	MACS2|mac2|Macs2)
+		U_arg='MACS2';;
+	*) 
+		echo "Error value : -U argument must be 'HOMER' or 'MACS2'"
+		exit;;
 esac
 
 # Deal with options [-N|-U|-S|-M|-I|-F|-L|-C|-T|-G|-H|-E] and arguments [$1|$2]
@@ -181,13 +181,13 @@ shift $((OPTIND-1))
 ################################################################################################################
 
 if [ $# -eq 1 ] && [ $1 == "help" ]; then
-    Help
-    exit
+	Help
+	exit
 elif [ $# -ne 2 ]; then
-    # Error if inoccrect number of agruments is provided
-    echo "Error synthax : please use following synthax"
-    echo "      sh ${script_name} [options] <input_dir> <chr_size_file>"
-    exit
+	# Error if inoccrect number of agruments is provided
+	echo "Error synthax : please use following synthax"
+	echo "      sh ${script_name} [options] <input_dir> <chr_size_file>"
+	exit
 elif [ $(ls $1/*${N_arg}*.bam 2>/dev/null | wc -l) -lt 1 ]; then
 	# Error if provided directory is empty or does not exists
 	echo 'Error : can not find files to align in provided directory. Please make sure the provided input directory exists, and contains matching .bam files.'
@@ -230,13 +230,13 @@ if [ ${U_arg} == 'HOMER' ]; then
 		# Create Peaks output directories
 		outdir=HOMER/Peaks/${current_tag}
 		mkdir -p ${outdir}
-	    
+
 		# Set variables for the run :
 		peaks_txt=${outdir}/${current_tag}_peaks.txt
 		peaks_bed=${outdir}/${current_tag}_peaks.bed
 		bedgraph=${outdir}/${current_tag}_peaks.bedgraph
 		bigwig=${outdir}/${current_tag}_peaks.bw
-	    
+
 		# Define JOBNAME and COMMAND and launch job
 		JOBNAME="HOMER_${current_tag}"
 		COMMAND="makeTagDirectory HOMER/Tags/${current_tag} ${file} -fragLength ${S_arg} -single \n\
@@ -251,7 +251,6 @@ if [ ${U_arg} == 'HOMER' ]; then
 		echo ",,,,,,${current_tag}.bam,${current_tag}_peaks.bed,bed" >> HOMER/SampleSheet_HOMER.csv
 	done
 
-
 ## MACS2 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 elif [ ${U_arg} == 'MACS2' ]; then
 	module load gcc
@@ -262,29 +261,28 @@ elif [ ${U_arg} == 'MACS2' ]; then
 	echo "ID,Tissue,Factor,Condition,Treatment,Replicate,bamReads,Peaks,PeakCaller" > MACS2/SampleSheet_MACS2.csv
 
 	# For each matching BAM file in $input directory
-        for file in ${1}/*${N_arg}*.bam; do    
-            # Define current tag
-            current_tag=`echo ${file} | sed -e "s@${1}/@@g" | sed -e "s@${N_arg}@@g" | sed -e 's@\.bam@@g'`
-            # Create output dir
-            outdir=MACS2/Peaks/${current_tag}
-            mkdir -p ${outdir}
-	    
-            # Set variables for the run :
-            narrrow_peak=${outdir}/${current_tag}_peaks.narrowPeak
-            summits_bed=${outdir}/${current_tag}_summits.bed
-            bedgraph=${outdir}/${current_tag}_summits.bedgraph
-            bigwig=${outdir}/${current_tag}_summits.bw
-	    
-            # Define JOBNAME and COMMAND and launch job
-            JOBNAME="MACS2_${current_tag}"
-            COMMAND="macs2 callpeak -t ${file} -f BAM -g ${G_arg} \
-            --nomodel --shift ${H_arg} --extsize ${E_arg} \
-            -n ${current_tag} --outdir ${outdir} \n\
-            genomeCoverageBed -bga -i ${summits_bed} -g ${2} | bedtools sort > ${bedgraph} \n\
-            bedGraphToBigWig ${bedgraph} ${1} ${bigwig}"
-            Launch
-            # Append SampleSheet
-	        echo ",,,,,,${current_tag}.bam,${current_tag}_peaks.bed,bed" >> MACS2/SampleSheet_MACS2.csv
-        done
+	for file in ${1}/*${N_arg}*.bam; do    
+		# Define current tag
+		current_tag=`echo ${file} | sed -e "s@${1}/@@g" | sed -e "s@${N_arg}@@g" | sed -e 's@\.bam@@g'`
+		# Create output dir
+		outdir=MACS2/Peaks/${current_tag}
+		mkdir -p ${outdir}
 
+		# Set variables for the run :
+		narrrow_peak=${outdir}/${current_tag}_peaks.narrowPeak
+		summits_bed=${outdir}/${current_tag}_summits.bed
+		bedgraph=${outdir}/${current_tag}_summits.bedgraph
+		bigwig=${outdir}/${current_tag}_summits.bw
+	    
+		# Define JOBNAME and COMMAND and launch job
+		JOBNAME="MACS2_${current_tag}"
+		COMMAND="macs2 callpeak -t ${file} -f BAM -g ${G_arg} \
+		--nomodel --shift ${H_arg} --extsize ${E_arg} \
+		-n ${current_tag} --outdir ${outdir} \n\
+		genomeCoverageBed -bga -i ${summits_bed} -g ${2} | bedtools sort > ${bedgraph} \n\
+		bedGraphToBigWig ${bedgraph} ${1} ${bigwig}"
+		Launch
+		# Append SampleSheet
+		echo ",,,,,,${current_tag}.bam,${current_tag}_peaks.bed,bed" >> MACS2/SampleSheet_MACS2.csv
+	done
 fi
