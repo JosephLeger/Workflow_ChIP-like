e5b8daa39b31146e777ff03214d0bb87cedfcc6c
JosephLeger <95336622+JosephLeger@users.noreply.github.com>
2024-05-16 09:45:26 +0200

    Update 1_QC.sh

--- script/1_QC.sh
+++ script/1_QC.sh
@@ -18,53 +18,39 @@ Help()
 {
 echo -e "${BOLD}####### QC MANUAL #######${END}\n\n\
 ${BOLD}SYNTHAX${END}\n\
-        sh ${script_name} <input_dir1> <...>\n\n\
+	sh ${script_name} <input_dir>\n\n\
 
 ${BOLD}DESCRIPTION${END}\n\
-        Perform quality check of FASTQ files using FastQC and merge results into a single file using MultiQC.\n\
-        It creates new folders './QC/<input_dir>' and './QC/MultiQC' in which quality check results are stored.\n\
-        Output files are .html files for direct visualization and .zip files containing results.\n\n\
+	Generate summarized quality check report for raw, trimmed or mapped files using MultiQC.\n\
+ 	If FASTQ files are provided, first launch FastQC to generate individual quality reports.\n\
+	It creates new folders './QC/<input_dir>' and './QC/MultiQC' in which quality check results are stored.\n\
+	Output files are HTML files for direct visualization and ZIP files containing results.\n\n\
 
 ${BOLD}ARGUMENTS${END}\n\
-        ${BOLD}<input_dir1>${END}\n\
-                Directory containing .fastq.gz or .fq.gz files to use as input for QC.\n\n\
-        ${BOLD}<...>${END}\n\
-                Several directories can be specified as argument in the same command line.\n\n\
-                
+	${BOLD}<input_dir1>${END}\n\
+		Directory containing QC reports files or FASTQ files.\n\n\
+  		Usually corresponds to 'Raw', 'Trimmed', 'STAR' or 'RSEM'.
+		
 ${BOLD}EXAMPLE USAGE${END}\n\
-        sh ${script_name} ${BOLD}Raw${END}\n"
+	sh ${script_name} ${BOLD}Raw${END}\n"
 }
 
 ################################################################################################################
 ### ERRORS -----------------------------------------------------------------------------------------------------
 ################################################################################################################
 
-# Count .fastq.gz or .fq.gz files in provided directory
-files=$(shopt -s nullglob dotglob; echo $1/*.fastq.gz $1/*.fq.gz)
-
 if [ $# -eq 1 ] && [ $1 == "help" ]; then
-        Help
-        exit
-elif [ $# -lt 1 ]; then
-        # Error if inoccrect number of agruments is provided
-        echo "Error synthax : please use following synthax"
-        echo "          sh ${script_name} <input_dir1> <...>"
-        exit
-else
-        input_list=''
-        # For each input file given as argument
-        for input in "$@"; do
-                # Precise to eliminate empty lists for the loop
-                shopt -s nullglob
-                for i in ${input}/*.fastq.gz ${input}/*.fq.gz; do
-                        input_list=${input_list}${i}' '
-                done
-        done
-        if (( !${#input_list} )); then
-                # Error if current provided directories are all empty
-                echo -e "Error : can not find any file in provided directories. Please make sure the provided input directories exist, and contain .fastq.gz or .fq.gz files."
-                exit
-        fi
+	Help
+	exit
+elif [ $# -ne 1 ]; then
+  # Error if inoccrect number of agruments is provided
+	echo "Error synthax : please use following synthax"
+	echo "          sh ${script_name} <input_dir>"
+	exit
+elif [ $(ls $1/*.fastq.gz $1/*.fq.gz $1/**/*.cnt $1/*.out 2>/dev/null | wc -l) -lt 1 ]; then		
+	# Error if provided directories are empty or don't exist
+ 	echo -e "Error : can not find any file in provided directories. Please make sure the provided input directories exist, and contain .fastq.gz or .fq.gz files."
+	exit
 fi
 
 ################################################################################################################
@@ -89,29 +75,30 @@ echo -e ${COMMAND} |  sed 's@^@   \| @' >> ./0K_REPORT.txt
 WAIT=''
 
 ## FASTQC - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-# Initialize JOBLIST to wait before running MultiQC
-JOBLIST='_'
-
-# For each input file given as argument
-for input in "$@"; do
-        # Create directory in QC folder following the same path than input path provided
-        outdir=QC/${input}
-        mkdir -p ${outdir}
-        # Generate jobname replacing '/' by '_'
-        name=`echo ${input} | sed -e 's@\/@_@g'`
-        # Precise to eliminate empty lists for the loop
-        shopt -s nullglob
-        # Launch FastQC for each provided file
-        for i in ${input}/*.fastq.gz ${input}/*.fq.gz; do
-                # Set variables for jobname
-                current_file=`echo $i | sed -e "s@${input}\/@@g" | sed -e "s@\.fastq\.gz\|\.fq\.gz@@g"`
-                # Define JOBNAME and COMMAND and launch job while append JOBLIST
-                JOBNAME="QC_${name}_${current_file}"
-                COMMAND="fastqc -o ${outdir} --noextract -f fastq $i"
+if [ $(ls $1/*.fastq.gz $1/*.fq.gz 2>/dev/null | wc -l) -gt 0 ]; then
+	# Initialize JOBLIST to wait before running MultiQC
+	JOBLIST='_'
+	# Create directory in QC folder following the same path than input path provided
+	outdir=QC/$1
+	mkdir -p ${outdir}
+	# Generate jobname replacing '/' by '_'
+	name=`echo $1 | sed -e 's@\/@_@g'`
+	# Precise to eliminate empty lists for the loop
+	shopt -s nullglob
+	# Launch FastQC for each provided file
+	for i in $1/*.fastq.gz $1/*.fq.gz; do
+		# Set variables for jobname
+		current_file=`echo $i | sed -e "s@$1\/@@g" | sed -e "s@\.fastq\.gz\|\.fq\.gz@@g"`
+		# Define JOBNAME and COMMAND and launch job while append JOBLIST
+		JOBNAME="QC_${name}_${current_file}"
+		COMMAND="fastqc -o ${outdir} --noextract -f fastq $i"
 		JOBLIST=${JOBLIST}','${JOBNAME}
-                Launch
-        done
-done
+		Launch
+	done
+ 	WAIT=`echo ${JOBLIST} | sed -e 's@_,@-hold_jid @'`
+else
+	outdir=$1
+fi
 
 ## MULTIQC - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Create directory in QC folder for MultiQC
@@ -120,8 +107,7 @@ mkdir -p ${outdir2}
 # Create output name without strating 'QC/' and replacing '/' by '_'
 name=`echo ${outdir} | sed -e 's@\/@_@g'`
 
-## Define JOBNAME, COMMAND and launch with WAIT list
+# Define JOBNAME, COMMAND and launch with WAIT list
 JOBNAME="MultiQC_${name}"
 COMMAND="multiqc ${outdir} -o ${outdir2} -n ${name}_MultiQC"
-WAIT=`echo ${JOBLIST} | sed -e 's@_,@-hold_jid @'`
 Launch
