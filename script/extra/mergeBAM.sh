bcb873a0481ec8a78269486eea28cf4e25ebb5a2
JosephLeger <95336622+JosephLeger@users.noreply.github.com>
2024-06-05 10:08:48 +0200

    Update mergeBAM.sh

--- script/extra/mergeBAM.sh
+++ script/extra/mergeBAM.sh
@@ -32,6 +32,10 @@ ${BOLD}OPTIONS${END}\n\
 	${BOLD}-R${END} ${UDL}boolean${END}, ${BOLD}R${END}emoveSuffix\n\
 		Specify whether provided suffix from input filename have to be removed in output filename.\n\
 		Default = false\n\n\
+  	${BOLD}-A${END} ${UDL}filename${END}, Merge${BOLD}A${END}ll\n\
+   		Specify an output filename to merge all matching BAM files into a single one.\n\
+     		If specified, it is not nescessary to specify <sheet_sample.csv> argument.\n\
+       		Default = None\n\n\
 
 ${BOLD}ARGUMENTS${END}\n\
 	${BOLD}<input_dir>${END}\n\
@@ -54,17 +58,20 @@ ${BOLD}EXAMPLE USAGE${END}\n\
 # Set default values
 N_arg='_filtered'
 R_arg='false'
+A_arg='None'
 
 # Change default values if another one is precised
-while getopts ":N:R:" option; do
+while getopts ":N:R:A:" option; do
 	case $option in
 		N) # NAME OF FILE (SUFFIX)
 			N_arg=${OPTARG};;
 		R) # REMOVE SUFFIX IN OUTPUT FILENAME
 			R_arg=${OPTARG};;
+   		A) # MERGE ALL BAM
+     			A_arg=${OPTARG};;
 		\?) # Error
 			echo "Error : invalid option"
-			echo "      Allowed options are [-N|-R]"
+			echo "      Allowed options are [-N|-R|-A]"
 			echo "      Enter 'sh ${script_name} help' for more details"
 			exit;;
 	esac
@@ -79,6 +86,15 @@ case $R_arg in
 	*) 
 		echo "Error value : -R argument must be 'true' or 'false'"
 		exit;;
+esac
+case $A_arg in
+	None|none|false|FALSE|False|F) 
+		A_arg='None';;
+	'')
+		echo "Error value : provided -A argument is empty"
+		exit;;
+	*) 
+		A_arg=`echo ${A_arg} | sed -e 's@\.bam|\.BAM|\.Bam@@'`
 esac
 # Deal with options [-N|-R] and arguments [$1|$2|...]
 shift $((OPTIND-1))
@@ -122,36 +138,44 @@ echo -e ${COMMAND} | sed 's@^@   \| @' >> ./0K_REPORT.txt
 }
 WAIT=''
 
-## MERGE BAM - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-# Establish conditions_list which contains already visited condition
-conditions_list=""
-sed 1d ${2} | while IFS=',' read -r id info condition; do
-	# Read the entire sheet condition columns
-	condition=$(echo $condition | tr -d '\r')
-	# Check if current $condition is already written in $conditions_list
-	is_present=`echo $conditions_list | grep -ce $condition`
+## MERGE SEPARATED BAM - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+if [ ${A_arg} == 'None' ]; then
+	# Establish conditions_list which contains already visited condition
+	conditions_list=""
+	sed 1d ${2} | while IFS=',' read -r id info condition; do
+		# Read the entire sheet condition columns
+		condition=$(echo $condition | tr -d '\r')
+		# Check if current $condition is already written in $conditions_list
+		is_present=`echo $conditions_list | grep -ce $condition`
+	
+		# If $condition is not written yet in $conditions_list
+		if [ $is_present -eq 0 ]; then
+			# Add it to the list to not visiting it again
+			conditions_list="${conditions_list} ${condition}"
+			
+			# Initialize $list_files to store filenames to merge
+			list_files=""
+			# Look for $id in the entire sheet that share current $condition
+			sed 1d ${2} | (while IFS=',' read -r id_sub info_sub condition_sub; do
+				id=$(echo $id | tr -d '\r')
+				condition_sub=$(echo $condition_sub | tr -d '\r')
+				if [ "$condition_sub" == "$condition" ]; then
+					# Search for $files correspoinding to current matching $id
+					newfile=`find ${1} -type f -iname "*${id_sub}*${N_arg}*.bam"`
+					list_files="${list_files} ${newfile}"
+				fi
+			done
+			# Define JOBNAME and COMMAND and launch job
+			JOBNAME="mergeBAM_${condition}"
+			COMMAND="samtools merge -o ${1}/${condition}${newsuffix} ${list_files} \n\
+			samtools index ${1}/${condition}${newsuffix}"
+			Launch)        
+		fi
+	done
+else
+## MERGE UNIQUE BAM - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+	# Initialize $list_files to store filenames to merge
+	list_files=`ls ${1}/*${N_arg}*.bam`
+	echo $list_files
+ fi
 
-	# If $condition is not written yet in $conditions_list
-	if [ $is_present -eq 0 ]; then
-		# Add it to the list to not visiting it again
-		conditions_list="${conditions_list} ${condition}"
-		
-		# Initialize $list_files to store filenames to merge
-		list_files=""
-		# Look for $id in the entire sheet that share current $condition
-		sed 1d ${2} | (while IFS=',' read -r id_sub info_sub condition_sub; do
-			id=$(echo $id | tr -d '\r')
-			condition_sub=$(echo $condition_sub | tr -d '\r')
-			if [ "$condition_sub" == "$condition" ]; then
-				# Search for $files correspoinding to current matching $id
-				newfile=`find ${1} -type f -iname "*${id_sub}*${N_arg}*.bam"`
-				list_files="${list_files} ${newfile}"
-			fi
-		done
-		# Define JOBNAME and COMMAND and launch job
-		JOBNAME="mergeBAM_${condition}"
-		COMMAND="samtools merge -o ${1}/${condition}${newsuffix} ${list_files} \n\
-		samtools index ${1}/${condition}${newsuffix}"
-		Launch)        
-	fi
-done
