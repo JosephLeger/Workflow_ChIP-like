8efe7810eaa6da4f9fb82ce4d6bf08c4168bd4ac
JosephLeger <95336622+JosephLeger@users.noreply.github.com>
2025-04-07 17:31:52 +0200

    Update 3_Bowtie2.sh -> V2

--- script/workflow/3_Bowtie2.sh
+++ script/workflow/3_Bowtie2.sh
@@ -138,21 +138,33 @@ fi
 ################################################################################################################
 
 ## SETUP - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-module load bowtie2/2.5.1
-module load samtools/1.15.1
-module load picard/2.23.5
-
 # Generate REPORT
 echo '#' >> ./0K_REPORT.txt
 date >> ./0K_REPORT.txt
 
 Launch()
 {
-# Launch COMMAND and save report
-echo -e "#$ -V \n#$ -cwd \n#$ -S /bin/bash \n""${COMMAND}" | qsub -N "${JOBNAME}" ${WAIT}
-echo -e "${JOBNAME}" >> ./0K_REPORT.txt
+# Launch COMMAND while getting JOBID
+JOBID=$(echo -e "#!/bin/bash \n\
+#SBATCH --job-name=${JOBNAME} \n\
+#SBATCH --output=%x_%j.out \n\
+#SBATCH --error=%x_%j.err \n\
+#SBATCH --time=${TIME} \n\
+#SBATCH --nodes=${NODE} \n\
+#SBATCH --ntasks=${TASK} \n\
+#SBATCH --cpus-per-task=${CPU} \n\
+#SBATCH --mem=${MEM} \n\
+#SBATCH --qos=${QOS} \n\
+source /home/${usr}/.bashrc \n\
+micromamba activate Workflow_ChIP-like \n""${COMMAND}" | sbatch --parsable --clusters nautilus ${WAIT})
+# Define JOBID and print launching message
+JOBID=`echo ${JOBID} | sed -e "s@;.*@@g"` 
+echo "Submitted batch job ${JOBID} on cluster nautilus"
+# Fill in 0K_REPORT file
+echo -e "${JOBNAME}_${JOBID}" >> ./0K_REPORT.txt
 echo -e "${COMMAND}" | sed 's@^@   \| @' >> ./0K_REPORT.txt
 }
+# Define default waiting list for sbatch as empty
 WAIT=''
 
 # Create output directories
@@ -160,6 +172,9 @@ model=`echo $3 | sed -r 's/^.*\/(.*)$/\1/'`
 mkdir -p ./Mapped/${model}/{BAM,STAT}
 
 ## BOWTIE2 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set up parameters for SLURM ressources
+TIME='0-01:00:00'; NODE='1'; TASK='1'; CPU='4'; MEM='10g'; QOS='quick'
+
 if [ $1 == "SE" ]; then
 	# Initialize JOBLIST to wait before running MultiQC
 	JOBLIST='_'
@@ -171,14 +186,10 @@ if [ $1 == "SE" ]; then
 		current_file=`echo $i | sed -e "s@${2}\/@@g" | sed -e 's@\.fastq\.gz\|\.fq\.gz@@g'`
 		# Define JOBNAME and COMMAND and launch job while append JOBLIST
 		JOBNAME="Bowtie2_${1}_${model}_${current_file}"
-		COMMAND="(bowtie2 -p 2 ${M_arg}${U_arg}\
-		-x $3 -U $i) 2> Mapped/${model}/STAT/Stat_${model}_${current_file}.txt | picard SortSam INPUT=/dev/stdin \
-		OUTPUT=Mapped/${model}/BAM/${current_file}_sorted.bam \
-		VALIDATION_STRINGENCY=LENIENT \
-		TMP_DIR=tmp \
-		SORT_ORDER=coordinate"
-		JOBLIST=${JOBLIST}','${JOBNAME}
+		COMMAND="(bowtie2 -p 4 ${M_arg}${U_arg}\
+		-x $3 -U $i | samtools sort -o  Mapped/${model}/BAM/${current_file}_sorted.bam) 3>&1 1>&2 2>&3 | tee Mapped/${model}/STAT/Stat_${model}_${current_file}.txt"
 		Launch
+		JOBLIST=${JOBLIST}':'${JOBID}
 	done
 	# Set location variables for summarizing stat results from SE
 	loc_depth='1p'
@@ -200,14 +211,14 @@ elif [ $1 == "PE" ]; then
 		R2=`echo $i | sed -e 's/_R1/_R2/g'`
 		# Define JOBNAME and COMMAND and launch job while append JOBLIST
 		JOBNAME="Bowtie2_${1}_${model}_${current_pair}"
-		COMMAND="(bowtie2 -p 2 -q ${M_arg}${U_arg}\
+		COMMAND="(bowtie2 -p 4 -q ${M_arg}${U_arg}\
 		-x $3 -1 ${R1} -2 ${R2}) 2> Mapped/${model}/STAT/Stat_${model}_${current_file}.txt | picard SortSam INPUT=/dev/stdin \
 		OUTPUT=Mapped/${model}/BAM/${current_pair}_sorted.bam \
 		VALIDATION_STRINGENCY=LENIENT \
 		TMP_DIR=tmp \
 		SORT_ORDER=coordinate"
-		JOBLIST=${JOBLIST}','${JOBNAME}
 		Launch    
+		JOBLIST=${JOBLIST}':'${JOBID}
 	done
 	# Set location variables for summarizing stat results from PE
 	loc_depth='1p'
@@ -217,9 +228,13 @@ elif [ $1 == "PE" ]; then
 fi
 
 ## SUMMARIZE STATS - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set up parameters for SLURM ressources
+TIME='0-00:03:00'; NODE='1'; TASK='1'; CPU='1'; MEM='1g'; QOS='quick'
+
 # Prepare summary file and WAIT list
 echo "Filename,SequencingDepth,Mono_MappedFragNum_${model},Multi_MappedFragNum_${model},Total_MappedFragNum_${model},AlignmentRate_${model}" > Mapped/${model}/STAT/Summary_Stats_${model}.csv
-WAIT=`echo ${JOBLIST} | sed -e 's@_,@-hold_jid @'`
+# Initialize WAIT based on JOBLIST (empty or not)
+WAIT=`echo ${JOBLIST} | sed -e 's@_@-d afterany@'`
 
 # Unset Shopt Builtin (necessary for the following Job)
 shopt -u nullglob
@@ -236,9 +251,3 @@ rate=\`cat \${stat} | sed -n '6p' | sed -e 's@ .*@@g'\` \n\
 echo "\${file},\${depth},\${mono_mapped},\${multi_mapped},\${mapped},\${rate}" >> Mapped/${model}/STAT/Summary_Stats_${model}.csv \n\
 done"
 Launch
-
-
-
-
-
-
