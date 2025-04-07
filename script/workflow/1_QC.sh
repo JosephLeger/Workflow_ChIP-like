98e8bff3f2ee0ca51686762e71757edddd2ce8b9
JosephLeger <95336622+JosephLeger@users.noreply.github.com>
2025-04-07 17:30:34 +0200

    Update 1_QC.sh -> V2

--- script/workflow/1_QC.sh
+++ script/workflow/1_QC.sh
@@ -58,23 +58,40 @@ fi
 ################################################################################################################
 
 ## SETUP - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-module load fastqc/0.11.9
-module load multiqc/1.13
-
 # Generate REPORT
 echo '#' >> ./0K_REPORT.txt
 date >> ./0K_REPORT.txt
 
 Launch()
 {
-# Launch COMMAND and save report
-echo -e "#$ -V \n#$ -cwd \n#$ -S /bin/bash \n""${COMMAND}" | qsub -N "${JOBNAME}" ${WAIT}
-echo -e "${JOBNAME}" >> ./0K_REPORT.txt
+# Launch COMMAND while getting JOBID
+JOBID=$(echo -e "#!/bin/bash \n\
+#SBATCH --job-name=${JOBNAME} \n\
+#SBATCH --output=%x_%j.out \n\
+#SBATCH --error=%x_%j.err \n\
+#SBATCH --time=${TIME} \n\
+#SBATCH --nodes=${NODE} \n\
+#SBATCH --ntasks=${TASK} \n\
+#SBATCH --cpus-per-task=${CPU} \n\
+#SBATCH --mem=${MEM} \n\
+#SBATCH --qos=${QOS} \n\
+source /home/${usr}/.bashrc \n\
+micromamba activate Workflow_ChIP-like \n""${COMMAND}" | sbatch --parsable --clusters nautilus ${WAIT})
+# Define JOBID and print launching message
+JOBID=`echo ${JOBID} | sed -e "s@;.*@@g"` 
+echo "Submitted batch job ${JOBID} on cluster nautilus"
+# Fill in 0K_REPORT file
+echo -e "${JOBNAME}_${JOBID}" >> ./0K_REPORT.txt
 echo -e "${COMMAND}" | sed 's@^@   \| @' >> ./0K_REPORT.txt
 }
+# Define default waiting list for sbatch as empty
 WAIT=''
 
 ## FASTQC - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Set up parameters for SLURM ressources
+TIME='0-00:30:00'; NODE='1'; TASK='1'; CPU='1'; MEM='2g'; QOS='quick'
+
+# Define required ressources for SLURM
 if [ $(ls $1/*.fastq.gz $1/*.fq.gz 2>/dev/null | wc -l) -gt 0 ]; then
 	# Initialize JOBLIST to wait before running MultiQC
 	JOBLIST='_'
@@ -89,13 +106,14 @@ if [ $(ls $1/*.fastq.gz $1/*.fq.gz 2>/dev/null | wc -l) -gt 0 ]; then
 	for i in $1/*.fastq.gz $1/*.fq.gz; do
 		# Set variables for jobname
 		current_file=`echo $i | sed -e "s@$1\/@@g" | sed -e "s@\.fastq\.gz\|\.fq\.gz@@g"`
-		# Define JOBNAME and COMMAND and launch job while append JOBLIST
+		# Define JOBNAME and COMMAND and launch job while append JOBID to JOBLIST
 		JOBNAME="QC_${name}_${current_file}"
 		COMMAND="fastqc -o ${outdir} --noextract -f fastq $i"
-		JOBLIST=${JOBLIST}','${JOBNAME}
 		Launch
+		JOBLIST=${JOBLIST}':'${JOBID}
 	done
- 	WAIT=`echo ${JOBLIST} | sed -e 's@_,@-hold_jid @'`
+	# Initialize WAIT based on JOBLIST (empty or not)
+	WAIT=`echo ${JOBLIST} | sed -e 's@_@-d afterany@'`
 else
 	outdir=$1
 fi
